6c2941ed0cc30d5841862e0a00a5e5694482f33f
danganhphu <phudanganh107@gmail.com>
2022-03-08 08:29:38 +0700

    thay đổi fun main

--- src/stratery/duck_example/DuckExMain.java
+++ src/stratery/duck_example/DuckExMain.java
@@ -2,6 +2,7 @@ package stratery.duck_example;
 
 public class DuckExMain {
     public static void main(String[] args) {
+        ///sửa
         VitXiem vx = new VitXiem();
         IFlyBehavior fly = new FlyNoWay();
         IQuackBehavior quack = new SQuack();
